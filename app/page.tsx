a688a7d99a5151ed6694f02c7f81d2f41d4a1929
Immaculate <emil.dhauwe@icloud.com>
2024-10-23 21:46:15 +0200

    Added Discord copying

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,40 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen p-8 pb-20 gap-16 sm:p-20 font-mono">
-      <main className="flex-grow">
-        <h1 className="font-extrabold text-2xl">Immaculate</h1>
-        <br />
-        <p>Average C# dev learning TS using next.js</p>
-        <p>Learning Arduino at school</p>
-        <br />
-        <p>If you want to check out my projects:</p>
-        <a href="https://github.com/LordImmaculate" target="_blank">Github</a>
-        <br />
-        <a href="https://discord.com/users/694817835209326652" target="_blank">Discord</a>
+    <div className="flex flex-col min-h-screen font-mono">
+      <main className="flex-grow p-2 sm:pl-40 sm:pt-20">
+        <h1 className="font-extrabold text-2xl mb-4">Immaculate</h1>
+        <div>Average C# dev learning TS using next.js</div>
+        <div className="mb-4">Learning Arduino at school</div>
+        <div className="mb-4">If you want to check out my projects:</div>
+        <div>
+          <a href="https://github.com/LordImmaculate" target="_blank" className="underline">Github</a>
+        </div>
+        <div>
+          <a href="#" onClick={copyClipboard} onMouseOut={resetText} id="discord" className="underline">Discord: LordImmaculate</a>
+        </div>
       </main>
       <footer className="flex flex-wrap items-center justify-center">
-        <p className="text-xs">© 2024 LordImmaculate. All rights reserved.</p>
+        <div className="text-xs">© 2024 LordImmaculate. All rights reserved.</div>
       </footer>
     </div>
   );
+}
+
+function copyClipboard() {
+  navigator.clipboard.writeText("LordImmaculate");
+
+  var discordElement = document.getElementById("discord");
+  if (discordElement) {
+    discordElement.innerHTML = "Copied!";
+  }
+}
+
+async function resetText() {
+  await new Promise(r => setTimeout(r, 500));
+  var discordElement = document.getElementById("discord");
+  if (discordElement) {
+    discordElement.innerHTML = "Discord: LordImmaculate";
+  }
 }
